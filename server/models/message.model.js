b7a18f1290007ab91be047a88faa0c29094799f1
Thisaru Supun <thisarusupun1314@gmail.com>
2024-05-28 22:35:01 +0530

    send message completed - messages and conversations

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -21,6 +21,6 @@ const messageSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const Message = mongoose.model("Message", userSchema);
+const Message = mongoose.model("Message", messageSchema);
 
 export default Message;
