a0ce4447e851345d81033b968716b5836782bfe8
Thisaru Supun <thisarusupun1314@gmail.com>
2024-05-28 17:19:59 +0530

    signup endpoint created

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,11 +1,60 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+// signup endpoint
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
-  } catch (error) {}
+
+    // check if passwords match
+    if (password != confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    // check if user already exists
+    if (user) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // select a randm profile picture based on gender
+    //https://avatar.iran.liara.run/public/boy?username=Scott
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controler", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const login = (req, res) => {
   res.send("Login user");
+  return res.json(req.body);
 };
 
 export const logout = (req, res) => {
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
