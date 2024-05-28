d7e96b8e5978d791f89dfd0c290ee4654c899c1d
Thisaru Supun <thisarusupun1314@gmail.com>
2024-05-28 23:57:44 +0530

    getting registered users created

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.route.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -23,3 +26,4 @@ app.get("/", (req, res) => console.log("Hello World"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
