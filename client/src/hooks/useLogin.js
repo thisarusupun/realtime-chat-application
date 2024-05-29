54b6a26c8a887173601431e5759ca8e2a62d9230
Thisaru Supun <thisarusupun1314@gmail.com>
2024-05-29 20:48:00 +0530

    logout functionality implemented

---
+++ client/src/hooks/useLogin.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
+
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+
+  const { setAuthUser } = useAuthContext();
+
+  const login = async ({ username, password }) => {
+    const success = handleInputErrors({
+      username,
+      password,
+    });
+
+    if (!success) return;
+
+    setLoading(true);
+    try {
+      const res = await axios.post("/api/auth/login", { username, password });
+
+      if (res.error) {
+        throw new Error(res.error);
+      }
+
+      localStorage.setItem("authuser", JSON.stringify(res.data));
+      setAuthUser(res.data);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { loading, login };
+};
+
+export default useLogin;
+
+const handleInputErrors = ({ username, password }) => {
+  if (!username || !password) {
+    toast.error("Please fill in all fields");
+    return false;
+  }
+  return true;
+};
