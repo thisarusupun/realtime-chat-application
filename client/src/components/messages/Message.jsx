e700ebb80bf08eeae1341e766329b50ce5cb1ae5
Thisaru Supun <thisarusupun1314@gmail.com>
2024-05-30 18:06:04 +0530

    online messagning completed

--- client/src/components/messages/Message.jsx
+++ client/src/components/messages/Message.jsx
@@ -5,18 +5,15 @@ import useConversation from "../../zustand/useConversation";
 const Message = ({ message }) => {
   const { authUser } = useAuthContext();
   const { selectedConversation } = useConversation();
-
   const fromMe = message.senderId === authUser._id;
-
   const chatClassName = fromMe ? "chat-end" : "chat-start";
-
   const profilePic = fromMe
     ? authUser.profilePic
     : selectedConversation.profilePic;
 
   const bubbleBgColor = fromMe ? "bg-blue-500" : "";
-
   const formattedTime = extractTime(message.createdAt);
+  const shakeClass = message.shouldShake ? "shake" : "";
 
   return (
     <div className={`chat ${chatClassName}`}>
@@ -25,7 +22,7 @@ const Message = ({ message }) => {
           <img src={profilePic} alt="chat bubble" />
         </div>
       </div>
-      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+      <div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass}`}>
         {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
--- server/socket/socket.js
+++ server/socket/socket.js
@@ -12,6 +12,10 @@ const io = new Server(server, {
   },
 });
 
+export const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId];
+};
+
 const userSocketMap = {};
 
 io.on("connection", (socket) => {
